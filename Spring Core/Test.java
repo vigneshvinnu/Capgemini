9b08662ce0f72910c8f8fc95eb877731db1f404c
vigneshvinnu <49729616+vigneshvinnu@users.noreply.github.com>
2021-02-04 13:45:00 +0530

    Create Test.java

---
+++ Spring Core/Test.java
@@ -0,0 +1,13 @@
+package com.assignment.valid;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+	ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
+	Customer customer=(Customer) context.getBean("Customer");
+	customer.details();
+	}
+}
