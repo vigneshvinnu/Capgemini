de7d5f6679fcef62c17d7bbfd944f8579d6375d3
vigneshvinnu <49729616+vigneshvinnu@users.noreply.github.com>
2021-03-07 14:39:05 +0530

    Add files via upload

---
+++ Web development/JavaScript/Objects/objects6.js
@@ -0,0 +1,6 @@
+var firstName="vinnu";
+var lastName="vignesh";
+var fullName=`${firstName} ${lastName}`;
+console.log(eval(new String(`${lastName},${firstName}`)));
+console.log(eval(new String(fullName)));
+console.log(fullName);
