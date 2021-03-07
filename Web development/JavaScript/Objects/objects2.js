5bb0ac6e723c0c1ea5540f05f5fa569aae2305b8
vigneshvinnu <49729616+vigneshvinnu@users.noreply.github.com>
2021-03-07 13:39:24 +0530

    Add files via upload

---
+++ Web development/JavaScript/Objects/objects2.js
@@ -0,0 +1,13 @@
+class Rectangle {
+    constructor(width,height){
+      this.width=width;
+      this.height=height;
+    }
+ getArea()
+ {
+ console.log(this.width*this.height);
+ }
+  }
+
+  var Rectangle1 = new Rectangle(100,100)
+  Rectangle1.getArea();
