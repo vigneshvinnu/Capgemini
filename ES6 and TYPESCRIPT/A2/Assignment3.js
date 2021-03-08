cacd8a369a1c72a009ce528e288ea73df6aeca26
vigneshvinnu <49729616+vigneshvinnu@users.noreply.github.com>
2021-03-08 13:53:43 +0530

    Add files via upload

---
+++ ES6 and TYPESCRIPT/A2/Assignment3.js
@@ -0,0 +1,25 @@
+function getNextArmstrong1(n) {
+    var result = 0;
+    if (n > 1000) {
+        throw 'Error occured number is greater than 1000';
+    }
+    for (var i = 1; i <= n; i++) {
+        var w = i;
+        while (i > 0) {
+            var remainder = i % 10;
+            result += remainder * remainder * remainder;
+            i /= 10;
+        }
+        if (result === w) {
+            console.log(w);
+            break;
+        }
+        result = 0;
+    }
+}
+try {
+    getNextArmstrong1(1153);
+}
+catch (e) {
+    console.error(e);
+}
