484d1d047799fb1fa07d814463ca450aa21a0829
vigneshvinnu <49729616+vigneshvinnu@users.noreply.github.com>
2021-04-01 17:38:44 +0530

    Add files via upload

---
+++ React/assignment-react/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+import A1 from './Assignments/A1';
+import A2 from './Assignment2/A2';
+import A3 from './Assignments3/A3';
+import A4 from './Assignments4/A4';
+import A5 from './Assignment5/A5';
+import A6 from './Assignment6/A6';
+function App() {
+  return (
+    <div>
+     <A1 />
+     <A2/>
+     <A3/>
+     <A4 />
+     <A5/>
+     <A6/>
+    </div>
+  );
+}
+
+export default App;
