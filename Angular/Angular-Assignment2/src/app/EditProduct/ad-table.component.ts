662c32a850d77fd2e903f0369d5a0ff7079239d4
vigneshvinnu <49729616+vigneshvinnu@users.noreply.github.com>
2021-04-01 17:17:50 +0530

    Add files via upload

---
+++ Angular/Angular-Assignment2/src/app/EditProduct/ad-table.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AdModel } from '../ad-model';
+import { ItemService } from '../services/item.service';
+
+@Component({
+  selector: 'app-ad-table',
+  templateUrl: './ad-table.component.html',
+  styleUrls: ['./ad-table.component.css']
+})
+export class AdTableComponent implements OnInit {
+
+  constructor(private adService:ItemService, private router:Router) { }
+  arrAds:AdModel[];
+  serviceItems:Observable<AdModel[]> = null;
+  public adArray:AdModel[] = [];
+  ngOnInit(): void {
+    this.serviceItems = this.adService.getAllAds();
+    this.serviceItems.subscribe(data=>this.arrAds=data);
+    
+  }
+  deleteItem(item:any){
+    console.log(item);
+    this.adService.deleteAd(item);
+    
+  }
+  updateItem(item:any){
+    this.adService.toEditItem(item);
+    console.log(item);
+    
+  }
+
+}
